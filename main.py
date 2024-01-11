4739897b158d44291f21b15037e652670f83dfef
JR-Hashira <113061197+JR-Hashira@users.noreply.github.com>
2024-01-11 13:09:45 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Jerome Reaux Jr.
+# Your First Python Application 
+# Python 1
+# Januany 11, 2024
+
+print('Anime Rankings:')
+print('1.Happy Sugar Life:12/10')
+print('2.Jujustu Kaisan:10/10')
+print('3.Attack on Titan:9.6/10')
+print('4.The Eminence in Shadow:9.5/10')
+print('5.Wandering Witch:The Journey of Elaina:9.1/10')
+print('6.DeathNote:9/10')
+print('7.Demon Slayer:8.9/10')
+print('8.Kinos Journey:8.7/10')
+print('9.Classroom of the Elite:8.5/10')
+print('10.Scissor Seven:8.3/10')
+
