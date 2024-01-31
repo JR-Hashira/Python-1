cf46d552d6d1f6df82e718340e1f5e5346f58882
JR-Hashira <113061197+JR-Hashira@users.noreply.github.com>
2024-01-31 16:56:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+
+# Jerome Reaux Jr.
+# 2/1/2024
+# Reading and Writing Files in Python
+
+#Getting user's name
+name = input("Hello, thank you for joining me today\nWhat is your name?\n")
+
+#print user's names
+print(name + "? Odd name but nonetheless, I'm happy you're here.\n")
+
+print("Here's the rundown:\nThe bad boss(Mom) had locked up the treasure(Gummies)\n")
+print("Our job is break into the vault, get the treasures.\n ")
+print("")
+
+#The code opens the text file
+with open('PythonTextPrint.txt') as file:
+    print(file.read())
+
+print(file.close)
+
+code = input(:)
+#If else statement to get inside the value
+if code == "2901"
+print("We're in, grab what you can get out")
+elif code != 2901
+print("You got it wrong!\n Reread the note and try again")
+return 16
+
